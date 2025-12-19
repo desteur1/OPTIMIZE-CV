8708925d681be02b8c22d955dc63c6fff2f5b0b1
Desteur Fall <shmurdanoah@gmail.come>
2025-12-19 16:33:36 +0100

    updated my CV

--- optimize-cv/src/pages/AboutMe.jsx
+++ optimize-cv/src/pages/AboutMe.jsx
@@ -14,20 +14,42 @@ const About = () => {
           <p>
             Passionné par l'informatique et les nouvelles technologies, j'ai
             suivi une formation d'<strong>intégrateur-développeur web</strong>{" "}
-            au CEF. Au cours de cette formation, j'ai pu acquérir des bases
-            solides pour travailler dans le domaine du{" "}
-            <strong>développeur web</strong>.
+            au CEF, où j'ai acquis des compétences solides en{" "}
+            <strong>développement web et en programmation</strong>, tant côté
+            front-end que back-end. Cette expérience m'a permis de maîtriser des
+            langages et frameworks modernes, tout en développant une
+            méthodologie rigoureuse pour produire un code propre, sécurisé et
+            conforme aux bonnes pratiques du secteur.
           </p>
 
           <p>
-            Basé à Paris, je suis en recherche d'un stage au sein d'une agence
-            digitale pour consolider ma formation de{" "}
-            <strong>développeur web Full stack</strong>.
+            Aujourd'hui, je souhaite orienter ma carrière vers la{" "}
+            <strong>cybersécurité</strong>, un domaine qui me passionne
+            particulièrement et où je souhaite mettre mes compétences techniques
+            au service de la protection des systèmes d'information et des
+            données sensibles. Je suis motivé par les défis liés à la sécurité
+            informatique, tels que la prévention des attaques, la gestion des
+            vulnérabilités, la mise en place de solutions de sécurité et le
+            respect des normes et réglementations.
           </p>
 
           <p>
-            J'accorde une attention particulière à la qualité du code que
-            j'écris et je respecte les bonnes pratiques du web.
+            Je suis à la recherche d'une{" "}
+            <strong>alternance en cybersécurité</strong> à Paris ou en région
+            parisienne, qui me permettra de mettre en pratique mes
+            connaissances, de continuer à apprendre aux côtés de professionnels
+            expérimentés et de contribuer activement à la sécurité et à la
+            fiabilité des infrastructures informatiques d'une entreprise
+            innovante
+          </p>
+          <p>
+            Curieux, rigoureux et déterminé, je m'investis pleinement dans tout
+            projet que j'entreprends et je suis particulièrement attentif à la
+            qualité du code, à la sécurité des systèmes et aux bonnes pratiques
+            du développement et de la cybersécurité. Mon objectif est de devenir
+            un professionnel capable de sécuriser efficacement les
+            environnements numériques et de participer activement à la
+            transformation digitale des entreprises.
           </p>
         </article>
       </div>
@@ -164,7 +186,7 @@ const About = () => {
               >
                 <div
                   className="progress-bar bg-primary"
-                  style={{ width: "800%" }}
+                  style={{ width: "80%" }}
                 ></div>
               </div>
             </dd>
@@ -181,6 +203,22 @@ const About = () => {
                 <div
                   className="progress-bar bg-info"
                   style={{ width: "65%" }}
+                ></div>
+              </div>
+            </dd>
+            <dt>WORDPRESS 80%</dt>
+            <dd>
+              <div
+                className="progress"
+                role="progressbar"
+                aria-label="Danger example"
+                aria-valuenow="100"
+                aria-valuemin="0"
+                aria-valuemax="100"
+              >
+                <div
+                  className="progress-bar bg-warning"
+                  style={{ width: "80%" }}
                 ></div>
               </div>
             </dd>
@@ -213,6 +251,87 @@ const About = () => {
                 <div
                   className="progress-bar bg-danger"
                   style={{ width: "60%" }}
+                ></div>
+              </div>
+            </dd>
+            <dt>PYTHON 65%</dt>
+            <dd>
+              {" "}
+              <div
+                className="progress"
+                role="progressbar"
+                aria-label="Success example"
+                aria-valuenow="25"
+                aria-valuemin="0"
+                aria-valuemax="100"
+              >
+                <div
+                  className="progress-bar bg-success"
+                  style={{ width: "65%" }}
+                ></div>
+              </div>
+            </dd>
+            <dt>DJANGO 70%</dt>
+            <dd>
+              <div
+                className="progress"
+                role="progressbar"
+                aria-label="Info example"
+                aria-valuenow="50"
+                aria-valuemin="0"
+                aria-valuemax="100"
+              >
+                <div
+                  className="progress-bar bg-info"
+                  style={{ width: "70%" }}
+                ></div>
+              </div>
+            </dd>
+            <dt>EXPRESS 60%</dt>
+            <dd>
+              <div
+                className="progress"
+                role="progressbar"
+                aria-label="Danger example"
+                aria-valuenow="100"
+                aria-valuemin="0"
+                aria-valuemax="100"
+              >
+                <div
+                  className="progress-bar bg-danger"
+                  style={{ width: "60%" }}
+                ></div>
+              </div>
+            </dd>
+            <dt>DOCKER 50%</dt>
+            <dd>
+              <div
+                className="progress"
+                role="progressbar"
+                aria-label="Danger example"
+                aria-valuenow="100"
+                aria-valuemin="0"
+                aria-valuemax="100"
+              >
+                <div
+                  className="progress-bar bg-primary"
+                  style={{ width: "50%" }}
+                ></div>
+              </div>
+            </dd>
+            <dt>N8N 50%</dt>
+            <dd>
+              <div
+                className="progress"
+                role="progressbar"
+                aria-label="Warning example"
+                aria-valuenow="75"
+                aria-valuemin="0"
+                aria-valuemax="100"
+              >
+                <div
+                  className="progress-bar bg-warning"
+                  style={{ width: "50%" }}
                 ></div>
               </div>
             </dd>
